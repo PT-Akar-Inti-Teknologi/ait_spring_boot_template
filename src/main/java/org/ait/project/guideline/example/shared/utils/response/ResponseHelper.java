aa886c586cd683c277a7c5ff5e40ff9369857850
bintang-dev <bintang@akarinti.tech>
2023-10-04 10:20:48 +0700

    style: remove transform and cleaning generic smeelcode

--- src/main/java/org/ait/project/guideline/example/shared/utils/response/ResponseHelper.java
+++ src/main/java/org/ait/project/guideline/example/shared/utils/response/ResponseHelper.java
@@ -1,59 +1,63 @@
 package org.ait.project.guideline.example.shared.utils.response;
 
-import java.util.List;
-
 import lombok.RequiredArgsConstructor;
 import org.ait.project.guideline.example.shared.constant.enums.ResponseEnum;
-import org.ait.project.guideline.example.shared.dto.template.ResponseCollection;
-import org.ait.project.guideline.example.shared.dto.template.ResponseDetail;
-import org.ait.project.guideline.example.shared.dto.template.ResponseError;
-import org.ait.project.guideline.example.shared.dto.template.ResponseTemplate;
-import org.ait.project.guideline.example.shared.utils.transform.ResponseTemplateTransform;
+import org.ait.project.guideline.example.shared.dto.template.*;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ResponseHelper {
 
-  private final ResponseMessageHelper responseMessageHelper;
+    private final ResponseMessageHelper responseMessageHelper;
 
-  private final ResponseTemplateTransform responseTransform;
+    /**
+     * a utility function that is used to generate detailed responses based on the AIT Standard
+     *
+     * @param responseEnum Enum Response
+     * @param body         Body Return
+     * @param <T>          Generic Type, for class body return
+     * @return ResponseEntity contain Template Response based on AIT Standard
+     */
+    public <T> ResponseEntity<ResponseTemplate<ResponseDetail<T>>> createResponseDetail(
+            ResponseEnum responseEnum, T body) {
+        return ResponseEntity.status(responseEnum.getHttpStatus())
+                .body(
+                        new ResponseTemplate<>(responseMessageHelper.getResponseSchema(responseEnum), new ResponseDetail<>(body))
+                );
+    }
 
-  /**
-   * a utility function that is used to generate detailed responses based on the AIT Standard
-   * @param responseEnum Enum Response
-   * @param body Body Return
-   * @param <T> Generic Type, for class body return
-   * @return ResponseEntity contain Template Response based on AIT Standard
-   */
-  public <T> ResponseEntity<ResponseTemplate<ResponseDetail<T>>> createResponseDetail(
-      ResponseEnum responseEnum, T body) {
-    return ResponseEntity.status(responseEnum.getHttpStatus())
-        .body(
-            responseTransform.templateDetail(responseMessageHelper.getResponseSchema(responseEnum), body)
-        );
-  }
+    public <T> ResponseEntity<ResponseTemplate<T>> createResponseError(
+            ResponseEnum responseEnum, T body) {
+        return ResponseEntity.status(responseEnum.getHttpStatus())
+                .body(
+                        new ResponseTemplate<>(responseMessageHelper.getResponseSchema(responseEnum), body)
+                );
+    }
 
-  public <T> ResponseEntity<ResponseTemplate<T>> createResponseError(
-      ResponseEnum responseEnum, T body) {
-    return ResponseEntity.status(responseEnum.getHttpStatus())
-        .body(
-            responseTransform.templateError(responseMessageHelper.getResponseSchema(responseEnum), body)
-        );
-  }
+    public <P, T> ResponseEntity<ResponseTemplate<ResponseCollection<T>>> createResponseCollection(
+            ResponseEnum responseEnum, Page<P> page,
+            List<T> contents) {
+        return ResponseEntity.status(responseEnum.getHttpStatus())
+                .body(
+                        new ResponseTemplate<>(responseMessageHelper.getResponseSchema(responseEnum), createCollection(page, contents))
+                );
+    }
 
-  public <T> ResponseEntity<ResponseTemplate<ResponseCollection<T>>> createResponseCollection(
-      ResponseEnum responseEnum, Page page,
-      List<T> contents) {
-    return ResponseEntity.status(responseEnum.getHttpStatus())
-        .body(
-            responseTransform.templateCollection(responseMessageHelper.getResponseSchema(responseEnum), page,contents)
-        );
-  }
+    private <P, T> ResponseCollection<T> createCollection(Page<P> page, List<T> contents) {
+        return new ResponseCollection<>(
+                new ResponseCollectionContent<>(Optional.ofNullable(page).map(
+                        pageableData -> new PaginationConfig(page.getNumber(),
+                                page.getSize(),
+                                page.getTotalElements())).orElse(null), contents));
+    }
 
-  public Object createResponseErrorTemplate(ResponseEnum invalidParam, ResponseError responseError) {
-    return responseTransform.templateError(responseMessageHelper.getResponseSchema(invalidParam), responseError);
-  }
+    public Object createResponseErrorTemplate(ResponseEnum invalidParam, ResponseError responseError) {
+        return new ResponseTemplate<>(responseMessageHelper.getResponseSchema(invalidParam), responseError);
+    }
 }
