0cef5f822a571a517ae71f3d6e65080282e98dfb
devop <fajar.hambali@akarinti.tech>
2024-05-20 10:20:13 +0700

    fix: clean smells code

--- src/main/java/org/ait/project/guideline/example/modules/banner/service/core/impl/BannerCoreImpl.java
+++ src/main/java/org/ait/project/guideline/example/modules/banner/service/core/impl/BannerCoreImpl.java
@@ -2,8 +2,6 @@ package org.ait.project.guideline.example.modules.banner.service.core.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.coobird.thumbnailator.Thumbnails;
-import org.ait.project.guideline.example.config.properties.ThumbnailsConfigProperties;
 import org.ait.project.guideline.example.modules.banner.dto.param.BannerParam;
 import org.ait.project.guideline.example.modules.banner.dto.response.BannerRes;
 import org.ait.project.guideline.example.modules.banner.exception.BannerNotFoundException;
@@ -13,7 +11,6 @@ import org.ait.project.guideline.example.modules.banner.service.adapter.query.Ba
 import org.ait.project.guideline.example.modules.banner.service.core.BannerCore;
 import org.ait.project.guideline.example.modules.banner.transform.BannerMapper;
 import org.ait.project.guideline.example.shared.constant.enums.ResponseEnum;
-import org.ait.project.guideline.example.shared.dto.template.MultipartImage;
 import org.ait.project.guideline.example.shared.dto.template.ResponseDetail;
 import org.ait.project.guideline.example.shared.dto.template.ResponseTemplate;
 import org.ait.project.guideline.example.shared.utils.response.ResponseHelper;
@@ -21,13 +18,6 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @Service
@@ -41,22 +31,16 @@ public class BannerCoreImpl implements BannerCore {
 
     private final BannerCommandAdapter bannerCommandAdapter;
 
-
     private final BannerMapper bannerMapper;
-
-    private final ThumbnailsConfigProperties thumbnailsConfigProperties;
 
     @Override
     public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> upload(BannerParam param) {
         // Create logic to upload file
-
         // Create logic to resize file to create thumbnail and upload it
-        MultipartFile file = resizeImage(param.getFile());
-        String imageThumbnail = file != null ? file.getOriginalFilename() : null ;
         Banner banner = bannerCommandAdapter.save(bannerMapper.convertToEntity(
                 param,
                 null,
-                imageThumbnail)
+                null)
         );
 
         return responseHelper.createResponseDetail(
@@ -112,28 +96,4 @@ public class BannerCoreImpl implements BannerCore {
         return responseHelper.createResponseDetail(ResponseEnum.SUCCESS, null);
     }
 
-    private MultipartFile resizeImage(MultipartFile file) {
-        MultipartImage multipartImage = null;
-        BufferedImage originalImage;
-
-        try {
-            File img = new File("pathFile");
-            originalImage = ImageIO.read(img );
-            log.info("Image : "+ originalImage);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        Thumbnails.of(originalImage)
-                .size(thumbnailsConfigProperties.getTargetHeight(), thumbnailsConfigProperties.getTargetHeight())
-                .outputFormat(thumbnailsConfigProperties.getFormat())
-                .outputQuality(thumbnailsConfigProperties.getQuality())
-                .toOutputStream(outputStream);
-
-            multipartImage = new MultipartImage(outputStream.toByteArray(), file.getName()+"-thumbnails",
-                    file.getOriginalFilename(), file.getContentType(), outputStream.size());
-
-        } catch (IOException e) {
-            log.error("Error File to Image");
-            e.printStackTrace();
-        }
-        return multipartImage;
-    }
 }
