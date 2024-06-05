051ac5b0b70f917699663526d9096c170056c472
bintang-dev <bintang@akarinti.tech>
2024-06-05 11:33:20 +0700

    build: fixing download --run-build

--- src/main/java/org/ait/project/guideline/example/modules/banner/service/core/impl/BannerCoreImpl.java
+++ src/main/java/org/ait/project/guideline/example/modules/banner/service/core/impl/BannerCoreImpl.java
@@ -1,13 +1,17 @@
 package org.ait.project.guideline.example.modules.banner.service.core.impl;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnails;
+import org.ait.project.guideline.example.blob.modules.storageengine.localstorage.service.StorageService;
 import org.ait.project.guideline.example.config.properties.ThumbnailsConfigProperties;
 import org.ait.project.guideline.example.modules.banner.dto.param.BannerParam;
 import org.ait.project.guideline.example.modules.banner.dto.response.BannerRes;
 import org.ait.project.guideline.example.modules.banner.exception.BannerNotFoundException;
-import org.ait.project.guideline.example.blob.modules.storageengine.localstorage.service.StorageService;
 import org.ait.project.guideline.example.modules.banner.model.jpa.entity.Banner;
 import org.ait.project.guideline.example.modules.banner.service.adapter.command.BannerCommandAdapter;
 import org.ait.project.guideline.example.modules.banner.service.adapter.query.BannerQueryAdapter;
@@ -29,126 +33,118 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class BannerCoreImpl implements BannerCore {
 
-    private final ResponseHelper responseHelper;
+  private final ResponseHelper responseHelper;
 
-    private final BannerQueryAdapter bannerQueryAdapter;
+  private final BannerQueryAdapter bannerQueryAdapter;
 
-    private final BannerCommandAdapter bannerCommandAdapter;
+  private final BannerCommandAdapter bannerCommandAdapter;
 
-    private final BannerMapper bannerMapper;
+  private final BannerMapper bannerMapper;
 
-    private final StorageService storageService;
+  private final StorageService storageService;
 
-    private final ThumbnailsConfigProperties thumbnailsProperties;
+  private final ThumbnailsConfigProperties thumbnailsProperties;
 
-    @Override
-    public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> upload(BannerParam param) {
-        String fileName = System.currentTimeMillis()+"_"+param.getFile().getOriginalFilename();
-        String imageFile = storageService.uploadFile(fileName, param.getFile(), thumbnailsProperties.getDirectory());
-        String thumbnailFile = storageService.uploadFile("Thumbnail_"+fileName, resizeImage(param.getFile()), thumbnailsProperties.getDirectory());
-        Banner banner = bannerCommandAdapter.save(bannerMapper.convertToEntity(
-                param,
-                imageFile,
-                thumbnailFile)
-        );
+  @Override
+  public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> upload(BannerParam param) {
+    String fileName = System.currentTimeMillis() + "_" + param.getFile().getOriginalFilename();
+    String imageFile =
+        storageService.uploadFile(fileName, param.getFile(), thumbnailsProperties.getDirectory());
+    String thumbnailFile =
+        storageService.uploadFile("Thumbnail_" + fileName, resizeImage(param.getFile()),
+            thumbnailsProperties.getDirectory());
+    Banner banner =
+        bannerCommandAdapter.save(bannerMapper.convertToEntity(param, imageFile, thumbnailFile));
 
-        return responseHelper.createResponseDetail(
-                ResponseEnum.SUCCESS,
-                bannerMapper.convertToRes(banner)
-        );
+    return responseHelper.createResponseDetail(ResponseEnum.SUCCESS,
+        bannerMapper.convertToRes(banner));
+  }
+
+  @Override
+  public ResponseEntity<Resource> download(String id) {
+    Banner banner = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
+    ByteArrayResource arrayResource =
+        storageService.downloadFile(banner.getImageFile(), thumbnailsProperties.getDirectory());
+    return responseHelper.createResponseOctet(banner.getImageFile(), arrayResource);
+  }
+
+  @Override
+  public ResponseEntity<Resource> downloadThumbnail(String id) {
+    Banner banner = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
+    ByteArrayResource arrayResource =
+        storageService.downloadFile(banner.getThumbnailFile(), thumbnailsProperties.getDirectory());
+    return responseHelper.createResponseOctet(banner.getThumbnailFile(), arrayResource);
+  }
+
+  @Override
+  @Transactional
+  public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> update(String id,
+                                                                            BannerParam param) {
+    Banner existingData = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
+
+    String fileName = System.currentTimeMillis() + "_" + param.getFile().getOriginalFilename();
+    String imageFile =
+        storageService.uploadFile(fileName, param.getFile(), thumbnailsProperties.getDirectory());
+    String thumbnailFile =
+        storageService.uploadFile("Thumbnail_" + fileName, resizeImage(param.getFile()),
+            thumbnailsProperties.getDirectory());
+
+    Banner updatedData = bannerCommandAdapter.save(
+        bannerMapper.update(existingData, param, imageFile, thumbnailFile));
+    deleteFileBanner(existingData);
+
+    return responseHelper.createResponseDetail(ResponseEnum.SUCCESS,
+        bannerMapper.convertToRes(updatedData));
+  }
+
+  @Override
+  @Transactional
+  public ResponseEntity<ResponseTemplate<ResponseDetail<Object>>> delete(String id) {
+    Banner existingData = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
+    bannerCommandAdapter.delete(existingData);
+    deleteFileBanner(existingData);
+
+    return responseHelper.createResponseDetail(ResponseEnum.SUCCESS, null);
+  }
+
+  @Override
+  public ResponseEntity<ResponseTemplate<ResponseCollection<BannerRes>>> getAllBanner(
+      Pageable pageable, BannerSpecParam bannerSpecParam) {
+    Page<BannerRes> appVersionRequests =
+        bannerQueryAdapter.getPage(pageable, bannerSpecParam).map(bannerMapper::convertToRes);
+    return responseHelper.createResponseCollection(ResponseEnum.SUCCESS, appVersionRequests,
+        appVersionRequests.getContent());
+  }
+
+  private MultipartFile resizeImage(MultipartFile file) {
+    MultipartImage multipartImage = null;
+    BufferedImage originalImage;
+
+    try {
+      originalImage = ImageIO.read(file.getInputStream());
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+      Thumbnails.of(originalImage)
+          .size(thumbnailsProperties.getTargetHeight(), thumbnailsProperties.getTargetHeight())
+          .outputFormat(thumbnailsProperties.getFormat())
+          .outputQuality(thumbnailsProperties.getQuality()).toOutputStream(outputStream);
+
+      multipartImage =
+          new MultipartImage(outputStream.toByteArray(), file.getName() + "-thumbnails",
+              file.getOriginalFilename(), file.getContentType(), outputStream.size());
+
+    } catch (IOException e) {
+      log.error("Failed to resize image", e);
     }
+    return multipartImage;
+  }
 
-    @Override
-    public ResponseEntity<Resource> download(String id) {
-        Banner banner = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
-        ByteArrayResource arrayResource = storageService.downloadFile(banner.getImageFile(), thumbnailsProperties.getDirectory());
-        return responseHelper.createResponseOctet(
-                banner.getImageFile(), arrayResource
-        );
-    }
-
-    @Override
-    public ResponseEntity<Resource> downloadThumbnail(String id) {
-        Banner banner = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
-        return responseHelper.createResponseOctet(
-                banner.getTitle(),
-                storageService.downloadFile(banner.getThumbnailFile(), thumbnailsProperties.getDirectory())
-        );
-    }
-
-    @Override
-    @Transactional
-    public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> update(String id, BannerParam param) {
-        Banner existingData = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
-
-        String fileName = System.currentTimeMillis()+"_"+param.getFile().getOriginalFilename();
-        String imageFile = storageService.uploadFile(fileName, param.getFile(), thumbnailsProperties.getDirectory());
-        String thumbnailFile = storageService.uploadFile("Thumbnail_"+fileName, resizeImage(param.getFile()), thumbnailsProperties.getDirectory());
-
-        Banner updatedData = bannerCommandAdapter.save(bannerMapper.update(
-                existingData,
-                param,
-                imageFile,
-                thumbnailFile)
-        );
-        deleteFileBanner(existingData);
-
-        return responseHelper.createResponseDetail(
-                ResponseEnum.SUCCESS,
-                bannerMapper.convertToRes(updatedData)
-        );
-    }
-
-    @Override
-    @Transactional
-    public ResponseEntity<ResponseTemplate<ResponseDetail<Object>>> delete(String id) {
-        Banner existingData = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
-        bannerCommandAdapter.delete(existingData);
-        deleteFileBanner(existingData);
-
-        return responseHelper.createResponseDetail(ResponseEnum.SUCCESS, null);
-    }
-
-    @Override
-    public ResponseEntity<ResponseTemplate<ResponseCollection<BannerRes>>> getAllBanner(Pageable pageable, BannerSpecParam bannerSpecParam) {
-        Page<BannerRes> appVersionRequests = bannerQueryAdapter.getPage(pageable, bannerSpecParam)
-                .map(bannerMapper::convertToRes);
-        return responseHelper.createResponseCollection(ResponseEnum.SUCCESS, appVersionRequests, appVersionRequests.getContent());
-    }
-
-    private MultipartFile resizeImage(MultipartFile file) {
-        MultipartImage multipartImage = null;
-        BufferedImage originalImage;
-
-        try {
-            originalImage = ImageIO.read(file.getInputStream());
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            Thumbnails.of(originalImage)
-                    .size(thumbnailsProperties.getTargetHeight(), thumbnailsProperties.getTargetHeight())
-                    .outputFormat(thumbnailsProperties.getFormat())
-                    .outputQuality(thumbnailsProperties.getQuality())
-                    .toOutputStream(outputStream);
-
-            multipartImage = new MultipartImage(outputStream.toByteArray(), file.getName()+"-thumbnails",
-                    file.getOriginalFilename(), file.getContentType(), outputStream.size());
-
-        } catch (IOException e) {
-            log.error("Failed to resize image", e);
-        }
-        return multipartImage;
-    }
-    private void deleteFileBanner(Banner existingData){
-        storageService.deleteFile(existingData.getImageFile(), thumbnailsProperties.getDirectory());
-        storageService.deleteFile(existingData.getThumbnailFile(), thumbnailsProperties.getDirectory());
-    }
+  private void deleteFileBanner(Banner existingData) {
+    storageService.deleteFile(existingData.getImageFile(), thumbnailsProperties.getDirectory());
+    storageService.deleteFile(existingData.getThumbnailFile(), thumbnailsProperties.getDirectory());
+  }
 }
