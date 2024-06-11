cdd3ff7cfb0da884bebb1b87fceb4ae730a0c298
bintang-dev <bintang@akarinti.tech>
2024-06-11 17:23:46 +0700

    fix: fix multipart param --run-build

--- src/main/java/org/ait/project/guideline/example/modules/banner/dto/param/BannerParam.java
+++ src/main/java/org/ait/project/guideline/example/modules/banner/dto/param/BannerParam.java
@@ -3,24 +3,19 @@ package org.ait.project.guideline.example.modules.banner.dto.param;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.beans.ConstructorProperties;
-
 @Data
 public class BannerParam {
 
-    private MultipartFile file;
+  private MultipartFile file;
 
-    private String title;
+  private String title;
 
-    private String description;
+  private String description;
 
-
-
-    @ConstructorProperties({"file", "title", "description"})
-    public BannerParam(MultipartFile file, String title, String description) {
-        this.file = file;
-        this.title = title;
-        this.description = description;
-    }
+  public BannerParam(MultipartFile file, String title, String description) {
+    this.file = file;
+    this.title = title;
+    this.description = description;
+  }
 
 }
--- src/main/java/org/ait/project/guideline/example/modules/banner/interfaces/rest/BannerController.java
+++ src/main/java/org/ait/project/guideline/example/modules/banner/interfaces/rest/BannerController.java
@@ -14,7 +14,14 @@ import org.ait.project.guideline.example.shared.dto.template.ResponseTemplate;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,46 +30,51 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class BannerController {
 
-    private final BannerCore bannerCore;
+  private final BannerCore bannerCore;
 
 
-    @Operation(summary = "API Post Banner")
-    @PostMapping
-    public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> upload(@ModelAttribute BannerParam bannerParam){
-        return bannerCore.upload(bannerParam);
-    }
+  @Operation(summary = "API Post Banner")
+  @PostMapping
+  public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> upload(
+      @RequestParam(value = "file", required = false) MultipartFile file,
+      @RequestParam(value = "title", required = false) String title,
+      @RequestParam(value = "description", required = false) String description) {
+    return bannerCore.upload(new BannerParam(file, title, description));
+  }
 
-    @Operation(summary = "API Download Banner")
-    @GetMapping("/download")
-    public ResponseEntity<Resource> download(@RequestParam("id") String id){
-        return bannerCore.download(id);
-    }
+  @Operation(summary = "API Download Banner")
+  @GetMapping("/download")
+  public ResponseEntity<Resource> download(@RequestParam("id") String id) {
+    return bannerCore.download(id);
+  }
 
-    @Operation(summary = "API Download Thumbnail Banner")
-    @GetMapping("/download-thumbnail")
-    public ResponseEntity<Resource> downloadThumbnail(@RequestParam("id") String id){
-        return bannerCore.downloadThumbnail(id);
-    }
+  @Operation(summary = "API Download Thumbnail Banner")
+  @GetMapping("/download-thumbnail")
+  public ResponseEntity<Resource> downloadThumbnail(@RequestParam("id") String id) {
+    return bannerCore.downloadThumbnail(id);
+  }
 
-    @Operation(summary = "API Update Banner")
-    @PutMapping
-    public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> updateBanner(
-            @RequestParam("id") String id,
-            @ModelAttribute BannerParam bannerParam){
-        return bannerCore.update(id, bannerParam);
-    }
+  @Operation(summary = "API Update Banner")
+  @PutMapping
+  public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> updateBanner(
+      @RequestParam("id") String id,
+      @RequestParam(value = "file", required = false) MultipartFile file,
+      @RequestParam(value = "title", required = false) String title,
+      @RequestParam(value = "description", required = false) String description) {
+    return bannerCore.update(id, new BannerParam(file, title, description));
+  }
 
-    @Operation(summary = "API Delete Banner")
-    @DeleteMapping
-    public ResponseEntity<ResponseTemplate<ResponseDetail<Object>>> deleteBanner(@RequestParam("id") String id){
-        return bannerCore.delete(id);
-    }
+  @Operation(summary = "API Delete Banner")
+  @DeleteMapping
+  public ResponseEntity<ResponseTemplate<ResponseDetail<Object>>> deleteBanner(
+      @RequestParam("id") String id) {
+    return bannerCore.delete(id);
+  }
 
-    @Operation(summary = "API to get All Banner")
-    @GetMapping("/all")
-    public ResponseEntity<ResponseTemplate<ResponseCollection<BannerRes>>> getAllVersion(
-            Pageable pageable,
-            BannerSpecParam versionParam) {
-        return bannerCore.getAllBanner(pageable, versionParam);
-    }
+  @Operation(summary = "API to get All Banner")
+  @GetMapping("/all")
+  public ResponseEntity<ResponseTemplate<ResponseCollection<BannerRes>>> getAllVersion(
+      Pageable pageable, BannerSpecParam versionParam) {
+    return bannerCore.getAllBanner(pageable, versionParam);
+  }
 }
