3de6cd97b9e7c61e79a61635ebe2d3c99ca795be
bintang-dev <bintang@akarinti.tech>
2024-09-24 10:09:08 +0700

    docs: add documentation

--- src/main/java/org/ait/project/guideline/example/modules/banner/service/core/impl/BannerCoreImpl.java
+++ src/main/java/org/ait/project/guideline/example/modules/banner/service/core/impl/BannerCoreImpl.java
@@ -65,6 +65,11 @@ public class BannerCoreImpl implements BannerCore {
 
   private final UrlBuilderUtils urlBuilderUtils;
 
+  /**
+   * Validate param banner before upload banner.
+   * @param bannerParam Banner param
+   * @param isUpdate Is update or not
+   */
   private void validateUploadParam(BannerParam bannerParam,boolean isUpdate) {
     validateParamImage(bannerParam.getFile(),isUpdate);
     validateTitle(bannerParam.getTitle());
@@ -72,12 +77,23 @@ public class BannerCoreImpl implements BannerCore {
     validateDeeplink(bannerParam.getDeeplink());
   }
 
+  /**
+   * Validate description param banner before upload banner.
+   * @param description description
+   * @throws DescriptionEmptyException if description is null or empty
+   */
   private void validateDescription(String description) {
     if (description == null || description.isEmpty()) {
       throw new DescriptionEmptyException();
     }
   }
 
+  /**
+   * Validate title param banner before upload banner.
+   * @param title Title
+   * @throws TitleEmptyException if title is null or empty
+   * @throws TitleLargerThanException if length of title more than 255
+   */
   private void validateTitle(String title) {
     if (title == null || title.isEmpty()) {
       throw new TitleEmptyException();
@@ -88,12 +104,25 @@ public class BannerCoreImpl implements BannerCore {
     }
   }
 
+  /**
+   * Validate deeplink param banner before upload banner.
+   * @param deeplink Deeplink
+   * @throws DeeplinkEmptyException if deeplink is null or empty
+   */
   private void validateDeeplink(String deeplink) {
     if (deeplink == null || deeplink.isEmpty()) {
       throw new DeeplinkEmptyException();
     }
   }
 
+  /**
+   * Validate param image before upload banner.
+   * @param file MultipartFile that contains image
+   * @param isUpdate boolean that indicate if the banner is update or create
+   * @throws BannerFileEmptyException if file is null or empty and it is not update
+   * @throws BanerSizeOverException if size of file more than 5MB
+   * @throws FileNotImageException if file is not image
+   */
   private void validateParamImage(MultipartFile file, boolean isUpdate) {
 
       if ((file == null || file.isEmpty()) && !isUpdate) {
@@ -113,6 +142,12 @@ public class BannerCoreImpl implements BannerCore {
       }
   }
 
+  /**
+   * Validate the content of a file to ensure it is an image file.
+   * @param file the file to be validated
+   * @throws FileContentNotImageException if the content of the file is not an image
+   * @throws BannerFileEmptyException if an IOException occurs while reading the file
+   */
   private void checkContentFile(MultipartFile file) {
     try {
       BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
@@ -124,6 +159,12 @@ public class BannerCoreImpl implements BannerCore {
     }
   }
 
+  /**
+   * Get detail banner by id.
+   * @param id the id of the banner
+   * @return ResponseEntity of ResponseTemplate with ResponseDetail of BannerRes
+   * @throws BannerNotFoundException if banner with given id is not found
+   */
   @Override
   public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> getDetailBanner(String id) {
     Banner banner = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
@@ -133,6 +174,20 @@ public class BannerCoreImpl implements BannerCore {
         bannerMapper.convertToRes(banner));
   }
 
+  /**
+   * Upload a banner.
+   * @param param the banner param
+   * @return ResponseEntity of ResponseTemplate with ResponseDetail of BannerRes
+   * @throws BannerFileEmptyException if an IOException occurs while reading the file
+   * @throws FileContentNotImageException if the content of the file is not an image
+   * @throws FileNotImageException if the file is not an image
+   * @throws DeeplinkEmptyException if deeplink is empty
+   * @throws DescriptionEmptyException if description is empty
+   * @throws TitleEmptyException if title is empty
+   * @throws TitleLargerThanException if title is larger than 50 characters
+   * @throws BanerSizeEmptyException if banner size is empty
+   * @throws BanerSizeOverException if banner size is over the limit
+   */
   @Override
   public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> upload(BannerParam param) {
     validateUploadParam(param,false);
@@ -140,7 +195,7 @@ public class BannerCoreImpl implements BannerCore {
     String imageFile = storageService.uploadFile(fileName, param.getFile(),
         applicationProperties.getThumbnailsProperties().getDirectory());
     String thumbnailFile =
-        storageService.uploadFile("Thumbnail_" + fileName, resizeImage(param.getFile()),
+        storageService.uploadFile("Thumbnail__" + fileName, resizeImage(param.getFile()),
             applicationProperties.getThumbnailsProperties().getDirectory());
     Banner banner =
         bannerCommandAdapter.save(bannerMapper.convertToEntity(param, imageFile, thumbnailFile));
@@ -150,6 +205,12 @@ public class BannerCoreImpl implements BannerCore {
         bannerMapper.convertToRes(banner));
   }
 
+  /**
+   * Download a banner by its id.
+   * @param id the id of the banner to download
+   * @return ResponseEntity of Resource with the image file
+   * @throws BannerNotFoundException if the banner is not found
+   */
   @Override
   public ResponseEntity<Resource> download(String id) {
     Banner banner = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
@@ -158,6 +219,12 @@ public class BannerCoreImpl implements BannerCore {
     return responseHelper.createResponseOctet(banner.getImageFile(), arrayResource);
   }
 
+  /**
+   * Download a thumbnail of a banner by its id.
+   * @param id the id of the banner to download the thumbnail
+   * @return ResponseEntity of Resource with the thumbnail image file
+   * @throws BannerNotFoundException if the banner is not found
+   */
   @Override
   public ResponseEntity<Resource> downloadThumbnail(String id) {
     Banner banner = bannerQueryAdapter.getById(id).orElseThrow(BannerNotFoundException::new);
@@ -167,6 +234,18 @@ public class BannerCoreImpl implements BannerCore {
   }
 
 
+  /**
+   * Update a banner by its id.
+   * @param id the id of the banner to update
+   * @param param the parameters to update the banner
+   * @return ResponseEntity of ResponseTemplate with the updated banner
+   * @throws BannerNotFoundException if the banner is not found
+   * @throws BannerFileEmptyException if the file is null or empty
+   * @throws BanerSizeEmptyException if the file size is empty
+   * @throws BanerSizeOverException if the file size is over the limit
+   * @throws DeeplinkEmptyException if the deeplink is null or empty
+   * @throws DescriptionEmptyException if the description is null or empty
+   */
   @Override
   @Transactional
   public ResponseEntity<ResponseTemplate<ResponseDetail<BannerRes>>> update(String id,
@@ -195,6 +274,12 @@ public class BannerCoreImpl implements BannerCore {
         bannerMapper.convertToRes(updatedData));
   }
 
+  /**
+   * Delete a banner by the given id
+   * @param id the id of the banner to be deleted
+   * @return ResponseEntity of ResponseTemplate with the deleted banner
+   * @throws BannerNotFoundException if the banner is not found
+   */
   @Override
   @Transactional
   public ResponseEntity<ResponseTemplate<ResponseDetail<Object>>> delete(String id) {
@@ -205,6 +290,12 @@ public class BannerCoreImpl implements BannerCore {
     return responseHelper.createResponseDetail(ResponseEnum.SUCCESS, null);
   }
 
+  /**
+   * Get all banners with filter and pagination
+   * @param pageable the pagination configuration
+   * @param bannerSpecParam the filter configuration
+   * @return ResponseEntity of ResponseTemplate with the collection of BannerRes
+   */
   @Override
   public ResponseEntity<ResponseTemplate<ResponseCollection<BannerRes>>> getAllBanner(
       Pageable pageable, BannerSpecParam bannerSpecParam) {
@@ -214,6 +305,10 @@ public class BannerCoreImpl implements BannerCore {
         bannerResPage.getContent());
   }
 
+  /**
+   * Get all active banners.
+   * @return ResponseEntity of ResponseTemplate with the collection of BannerRes
+   */
   @Override
   public ResponseEntity<ResponseTemplate<ResponseCollection<BannerRes>>> getAllActiveBanner() {
     List<BannerRes> bannerResList =
@@ -221,12 +316,26 @@ public class BannerCoreImpl implements BannerCore {
     return responseHelper.createResponseCollection(ResponseEnum.SUCCESS, null, bannerResList);
   }
 
+  /**
+   * Adds image and thumbnail download URLs to the given BannerRes.
+   * @param response the BannerRes to add URLs to
+   * @return the BannerRes with the added URLs
+   */
   private BannerRes additional(BannerRes response) {
     response.setImageFile(urlBuilderUtils.createUrlDownloadImage(response.getImageFile()));
     response.setThumbnailFile(urlBuilderUtils.createUrlDownloadImage(response.getThumbnailFile()));
     return response;
   }
 
+  /**
+   * Resize the given MultipartFile image based on the application properties.
+   * The following properties are used:
+   * - targetHeight: The height of the resized image.
+   * - format: The image format of the resized image.
+   * - quality: The quality of the resized image.
+   * @param file The MultipartFile image to resize.
+   * @return The resized MultipartFile image.
+   */
   private MultipartFile resizeImage(MultipartFile file) {
     MultipartImage multipartImage = null;
     BufferedImage originalImage;
@@ -251,6 +360,18 @@ public class BannerCoreImpl implements BannerCore {
     return multipartImage;
   }
 
+  /**
+   * This method is used to sort the banner data. The banner data should be wrapped
+   * inside a List of BannerSortReq. The banner data is sorted based on the index
+   * value of the BannerSortReq. The banner that has the lowest index value will
+   * be placed first.
+   * @param bannerSortReqs The list of BannerSortReq that contains the banner
+   * data to be sorted.
+   * @return The response of the sorting banner operation. If the sorting is
+   * successful, the response will be a successful response with a message of
+   * "Success". If the sorting is failed, the response will be a failed response
+   * with a message of "Failed".
+   */
   @Override
   public ResponseEntity<ResponseTemplate<ResponseDetail<String>>> sortingBanner(
       List<BannerSortReq> bannerSortReqs) {
@@ -261,6 +382,14 @@ public class BannerCoreImpl implements BannerCore {
     return responseHelper.createResponseDetail(ResponseEnum.SUCCESS, "Success");
   }
 
+  /**
+   * Delete the banner image and thumbnail from the file system.
+   *
+   * <p>This method is used to delete the banner image and thumbnail from the file
+   * system when the banner is deleted.
+   *
+   * @param existingData The banner data.
+   */
   private void deleteFileBanner(Banner existingData) {
     storageService.deleteFile(existingData.getImageFile(),
         applicationProperties.getThumbnailsProperties().getDirectory());
